406febbbe31ccf1bf75fb35ce1cc30f5b82221cc
Sean Hawkridge <sean.hawkridge@alliants.com>
2016-08-22 15:20:37 +0100

    Passing test for 1 bottle

--- lib/99_bottles.rb
+++ lib/99_bottles.rb
@@ -8,6 +8,11 @@ class Bottles
         "2 bottles of beer.\n" +
         "Take one down and pass it around, " +
         "1 bottle of beer on the wall.\n"
+      when 1
+        "1 bottle of beer on the wall, " +
+        "1 bottle of beer.\n" +
+        "Take one down and pass it around, " +
+        "no more bottles of beer on the wall.\n"
       else
         "#{number} bottles of beer on the wall, " +
         "#{number} bottles of beer.\n" +
--- test/99_bottles_test.rb
+++ test/99_bottles_test.rb
@@ -31,6 +31,15 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verse(2)
   end
 
+  def test_verse_1
+    expected = "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Take one down and pass it around, " +
+    "no more bottles of beer on the wall.\n"
+
+    assert_equal expected, Bottles.new.verse(1)
+  end
+
 
 
 end
